1e6d395ef27ca4acfa7ecc281667e8aa6cca1812
Guilherme Tâmega <guitamega06@gmail.com>
2023-02-20 13:19:10 +0000

    Radio class study

--- origamid/src/components/Forms/FormUtils/index.jsx
+++ origamid/src/components/Forms/FormUtils/index.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 export default function FormUtils() {
   const [message, setMessage] = useState("");
   const [select, setSelect] = useState("");
+
+  const [radio, setRadio] = useState("");
+
+  function handleChange({ target }) {
+    setRadio(target.value);
+  }
   return (
     <>
       <form>
@@ -26,6 +32,34 @@ export default function FormUtils() {
           <option value="tablet">Tablet</option>
         </select>
         <p>{select}</p>
+        <hr />
+        <label>
+          <input
+            type="radio"
+            value="notebook"
+            checked={radio === "notebook"}
+            onChange={handleChange}
+          />
+          Notebook
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="smartphone"
+            checked={radio === "smartphone"}
+            onChange={handleChange}
+          />
+          Smartphone
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="tablet"
+            checked={radio === "tablet"}
+            onChange={handleChange}
+          />
+          Tablet
+        </label>
       </form>
       <hr />
     </>
